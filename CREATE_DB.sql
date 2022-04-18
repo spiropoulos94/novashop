b01058605a7e8c21ddb38f2720a65c65a97b57a6
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-04-19 00:54:53 +0300

    next

--- CREATE_DB.sql
+++ CREATE_DB.sql
@@ -53,6 +53,16 @@ CREATE TABLE Products (
     FOREIGN KEY (category_id) REFERENCES Categories(id)
 );
 
+CREATE TABLE Suppliers (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (address_id) REFERENCES Addresses(id)
+);
+
 CREATE TABLE Shops (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -66,7 +76,9 @@ CREATE TABLE Orders (
     client_id int NOT NULL,
     shop_id int NOT NULL,
     cost DECIMAL(10, 2) NOT NULL,
+    product_id int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (client_id) REFERENCES Clients(id),
+    FOREIGN KEY (product_id) REFERENCES Products(id),
     FOREIGN KEY (shop_id) REFERENCES Shops(id)
 );
